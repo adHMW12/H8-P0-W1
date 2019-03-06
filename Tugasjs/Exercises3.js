bbd0574dbd863ec029ebc55399dc7d8acc1f288f
adhmw <adehermawan631@gmail.com>
2019-03-06 23:08:57 +0700

    Tugas JS

---
+++ Tugasjs/Exercises3.js
@@ -0,0 +1,17 @@
+var nama='';
+var peran='';
+
+if(nama ==''){
+  console.log('Nama Harus di isi');
+}else if(peran ==''){
+  console.log('Halo' +nama+ ', Pilih peranmu untuk memulai game!');
+}else if(nama !=='' && peran =='Ksatria'){
+  console.log('Selamat datang di Dunia Proxytia,' +nama+'.');
+  console.log( 'Halo Ksatria' +nama+ ', kamu dapat menyerang dengan senjatamu!');
+}else if(nama !=='' && peran =='Tabib'){
+  console.log('Selamat datang di Dunia Proxytia,' +nama+'.');
+  console.log('Halo Tabib' +nama+ ', kamu akan membantu temanmu yang terluka.');
+}else if(nama!=='' && peran == 'Penyihir'){
+  console.log('Selamat datang di Dunia Proxytia,' +nama+'.');
+  console.log('Halo Penyihir' +nama+ ', ciptakan keajaiban yang membantu kemenanganmu!');
+}
